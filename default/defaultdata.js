ac6bc5d2276c4b79541cff9cfe8446a2c009ba6e
Jaejin Jang <jaejin0me@gmail.com>
2020-06-14 15:55:15 +0900

    시 추가하기 #9 default data추가를 위한 스크립트 추가

---
+++ default/defaultdata.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require('mongodb');
+
+async function main() {
+	const uri = "mongodb://localhost:27017?useUnifiedTopology=true";
+	const client = new MongoClient(uri);
+
+	try {
+		// Connect to the MongoDB cluster
+		await client.connect();
+
+		// Make the appropriate DB calls
+		await listDatabases(client);
+
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await client.close();
+	}
+}
+
+async function listDatabases(client) {
+	databasesList = await client.db().admin().listDatabases();
+	console.log("Databases:");
+	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
